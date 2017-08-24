ec50dd37c78a0d7b3fa524e50d3fd876875e1918
gelogelodin <bantiling.angelo@gmail.com>
2017-08-24 18:30:19 +0800

    Adjusted achievement input to select

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1073,6 +1073,7 @@
                                 <div class="youtube-url">
                                     <span>Insert/Paste Youtube Video Url:</span>
                                     <input type="" name="" placeholder="https://www.youtube.com/watch?v=SqMjxAWfTe4">
+                                    <span class="input-error">error</span>
                                 </div>
                                 <div class="radio-choices">
                                     <span class="title">Select video type to publish:</span>
@@ -1415,8 +1416,57 @@
                                         </div>
                                         <div class="input-group-half">
                                             <label>Date Received</label>
-                                            <input type="text" id="Month" name="Month" placeholder="January">
-                                            <input type="text" id="Day" name="Day" placeholder="01">
+                                            <div class="native-select" id="Month">
+                                                <select name="Month" placeholder="January">
+                                                    <option>January</option>
+                                                    <option>February</option>
+                                                    <option>March</option>
+                                                    <option>April</option>
+                                                    <option>May</option>
+                                                    <option>June</option>
+                                                    <option>July</option>
+                                                    <option>August</option>
+                                                    <option>September</option>
+                                                    <option>October</option>
+                                                    <option>November</option>
+                                                    <option>December</option>
+                                                </select>
+                                            </div>
+                                            <div class="native-select" id="Day">
+                                                <select name="Day" placeholder="01">
+                                                    <option>01</option>
+                                                    <option>02</option>
+                                                    <option>03</option>
+                                                    <option>04</option>
+                                                    <option>05</option>
+                                                    <option>06</option>
+                                                    <option>07</option>
+                                                    <option>08</option>
+                                                    <option>09</option>
+                                                    <option>10</option>
+                                                    <option>11</option>
+                                                    <option>12</option>
+                                                    <option>13</option>
+                                                    <option>14</option>
+                                                    <option>15</option>
+                                                    <option>16</option>
+                                                    <option>17</option>
+                                                    <option>18</option>
+                                                    <option>19</option>
+                                                    <option>20</option>
+                                                    <option>21</option>
+                                                    <option>22</option>
+                                                    <option>23</option>
+                                                    <option>24</option>
+                                                    <option>25</option>
+                                                    <option>26</option>
+                                                    <option>27</option>
+                                                    <option>28</option>
+                                                    <option>29</option>
+                                                    <option>30</option>
+                                                    <option>31</option>
+                                                </select>
+                                            </div>
                                             <input type="text" id="Year" name="Year" placeholder="2000">
                                         </div>
                                     </div>
